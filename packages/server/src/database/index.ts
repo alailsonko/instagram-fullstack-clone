c777788f89b6dc30646308b3fd6ef3c3c15df1df
alailsonko <aalailson3@gmail.com>
2020-09-02 20:36:35 -0300

    fix: ensure that the migration is working

--- packages/server/src/database/index.ts
+++ packages/server/src/database/index.ts
@@ -1,16 +1,3 @@
 import { createConnection } from 'typeorm'
 
-createConnection({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'tracking-system',
-  entities: ['/entities/*.js'],
-  synchronize: true
-})
-  .then(connection => {
-    // here you can start to work with your entities
-  })
-  .catch(error => console.log(error))
+createConnection()
