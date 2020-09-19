e459f86a3f1331a721769090b61967033d14221c
alailsonko <aalailson3@gmail.com>
2020-09-19 10:58:44 -0300

    feat: ensure emailvalidatoradapter returns true or false

---
+++ packages/server/src/utils/email-validator-adapter.ts
@@ -0,0 +1,9 @@
+import { EmailValidator } from '../presentation/protocols/email-validator'
+import validator from 'validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid(email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
+export default EmailValidatorAdapter
