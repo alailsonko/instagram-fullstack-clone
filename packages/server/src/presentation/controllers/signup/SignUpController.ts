a4852b1a500bc705758c71cb6b375a5a8d0a0aa8
alailsonko <aalailson3@gmail.com>
2020-09-18 22:53:41 -0300

    feat: ensure signupcontroller returns 400 if no email is provided

--- packages/server/src/presentation/controllers/signup/SignUpController.ts
+++ packages/server/src/presentation/controllers/signup/SignUpController.ts
@@ -3,19 +3,26 @@ import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class SignUpController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    try {
-      if (!httpRequest.body.username) {
-        const { statusCode, body } = {
-          statusCode: 400,
-          body: {
-            message: new Error('invalid username')
-          }
-        }
-        return {
-          statusCode,
-          body
-        }
+    if (!httpRequest.body.username) {
+      const { statusCode, body } = {
+        statusCode: 400,
+        body: { message: new Error('invalid username') }
       }
-    } catch (error) {}
+      return {
+        statusCode,
+        body
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      const { statusCode, body } = {
+        statusCode: 400,
+        body: { message: new Error('invalid email') }
+      }
+      return {
+        statusCode,
+        body
+      }
+    }
   }
 }
