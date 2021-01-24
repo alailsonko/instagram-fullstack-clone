2aa827c8b5f923bd0a9d125d79784ee650f96caf
alailsonko <aalailson3@gmail.com>
2021-01-24 10:04:05 -0300

    chore: implements router

---
+++ packages/server/src/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import authRouters from './auth'
+
+const routes = Router()
+
+const routesHandler = [authRouters]
+
+for (const rt of routesHandler) {
+  routes.use('/api/v1/', rt)
+}
+
+export default routes
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import routes from './routes'
 const app = express()
 
 dotenv.config()
-
+app.use(routes)
 app.listen(process.env.PORT, ()=> {
-    console.log(`Listening on port ${process.env.PORT}}`)
+    console.log(`Listening on port ${process.env.PORT}`)
 })
