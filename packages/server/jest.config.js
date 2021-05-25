b4d1be9bc526146bc78b4f2f6872ecfd53a18385
alailsonko <aalailson3@gmail.com>
2021-05-25 15:33:31 -0300

    fix: change extension of jest

---
+++ packages/server/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  clearMocks: true,
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
