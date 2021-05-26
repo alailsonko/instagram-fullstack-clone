3fdb9d0a9ef43f16e301bd34c98cc7a0f75ac5a9
alailsonko <aalailson3@gmail.com>
2021-05-25 21:08:40 -0300

    feat: implements tests

--- packages/server/src/controllers/auth/__tests__/signup.controller.spec.ts
+++ packages/server/src/controllers/auth/__tests__/signup.controller.spec.ts
@@ -10,6 +10,39 @@ describe('SignUp Controller', () => {
       passwordConfirmation: 'valid_password'
     }
     const response = sut.handle(user)
-    expect(response).toBe({ error: 'Invalid username' })
+    expect(response).toStrictEqual({ error: 'username must be provided' })
+  })
+  test('should throw error case email is empty', () => {
+    const sut = new SignUpController()
+    const user = {
+      username: 'valid_username',
+      email: '',
+      password: 'valid_password',
+      passwordConfirmation: 'valid_password'
+    }
+    const response = sut.handle(user)
+    expect(response).toStrictEqual({ error: 'email must be provided' })
+  })
+  test('should throw error case password is empty', () => {
+    const sut = new SignUpController()
+    const user = {
+      username: 'valid_username',
+      email: 'valid_email@example.com',
+      password: '',
+      passwordConfirmation: 'valid_password'
+    }
+    const response = sut.handle(user)
+    expect(response).toStrictEqual({ error: 'password must be provided' })
+  })
+  test('should throw error case passwordConfirmation is empty', () => {
+    const sut = new SignUpController()
+    const user = {
+      username: 'valid_username',
+      email: 'valid_email@example.com',
+      password: 'valid_password',
+      passwordConfirmation: ''
+    }
+    const response = sut.handle(user)
+    expect(response).toStrictEqual({ error: 'passwordConfirmation must be provided' })
   })
 })
--- packages/server/src/controllers/auth/signup.controller.ts
+++ packages/server/src/controllers/auth/signup.controller.ts
@@ -3,9 +3,37 @@ interface Controller {
   handle: (data: any) => any
 }
 
+interface AddAccount {
+  username: string
+  email: string
+  password: string
+  passwordConfirmation: string
+}
+
+interface ErrorResponse {
+  error: string
+}
+
+interface OkResponse {
+  ok: string
+}
+
 class SignUpController implements Controller {
-  handle (data: any): any {
-    return 'ok'
+  handle (data: AddAccount): ErrorResponse | OkResponse {
+    const { username, email, password, passwordConfirmation } = data
+    if (!username) {
+      return { error: 'username must be provided' }
+    }
+    if (!email) {
+      return { error: 'email must be provided' }
+    }
+    if (!password) {
+      return { error: 'password must be provided' }
+    }
+    if (!passwordConfirmation) {
+      return { error: 'passwordConfirmation must be provided' }
+    }
+    return { ok: 'User succesfull created' }
   }
 }
 
