2aa7222533629a0880e11bfdce4a34087a656315
alailsonko <aalailson3@gmail.com>
2021-09-18 20:44:35 -0300

    fix: test bcrypt

--- packages/server/src/infra/criptography/bcrypt.adapter.spec.ts
+++ packages/server/src/infra/criptography/bcrypt.adapter.spec.ts
@@ -28,7 +28,9 @@ describe('Bcrypt Adapter', () => {
   })
   test('Should throw if bcrypt throws', async () => {
     const sut = makeSut()
-    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest.spyOn(bcrypt, 'hash').mockImplementation(() => {
+      throw new Error()
+    })
     const promise = sut.encrypt('any_value')
     await expect(promise).rejects.toThrow()
   })
