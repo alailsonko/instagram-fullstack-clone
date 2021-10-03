c8f8c7af02884c2d680c1acc629f2823f7d7763f
alailsonko <aalailson3@gmail.com>
2021-10-02 22:01:04 -0300

    start context

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,12 +2,18 @@ import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
 import schema from './graphql/schemasMap'
 import dotenv from 'dotenv'
+import { ContextGraphQL } from './domain/auth/context'
 
 dotenv.config()
 
 const app = express()
 const server = new ApolloServer({
-  schema
+  schema,
+  context: ({ req }): ContextGraphQL => {
+    const token = req.headers.authorization ?? ''
+    console.log(token)
+    return { token }
+  }
 })
 
 server.applyMiddleware({ app, path: '/graphql' })
