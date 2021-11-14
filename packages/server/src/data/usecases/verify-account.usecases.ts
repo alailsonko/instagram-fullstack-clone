489d72ded8021de16ef74add200de9474ac6fc41
alailson <aalailson3@gmail.com>
2021-11-14 13:41:49 -0300

    refactor

--- packages/server/src/data/usecases/verify-account.usecases.ts
+++ packages/server/src/data/usecases/verify-account.usecases.ts
@@ -4,29 +4,29 @@ import { O } from "ts-toolbelt";
 import UserRepository from "../../infra/repositories/users/users.repositories";
 import { QueryLoginArgs } from "../../main/graphql/generated";
 
-type CompulsoryQueryLogin = O.Compulsory<QueryLoginArgs, "email" | 'username'>
+type CompulsoryQueryLogin = O.Compulsory<QueryLoginArgs, "email" | "username">;
 
 interface IVerifyAccount {
-  verify(data: O.Optional<CompulsoryQueryLogin, 'password'>): Promise<boolean>
+  verify(data: O.Optional<CompulsoryQueryLogin, "password">): Promise<boolean>;
 }
-
 
 class VerifyAccount implements IVerifyAccount {
-  private userRepository: UserRepository
+  private userRepository: UserRepository;
   constructor(userRepository: UserRepository) {
-    this.userRepository = userRepository
+    this.userRepository = userRepository;
   }
-  async verify(data: O.Optional<CompulsoryQueryLogin, 'password'>): Promise<boolean> {
-  const user = await this.userRepository.find({
-    email: data.email,
-    username: data.username
-  })
-  if(!user) {
-    throw new AuthenticationError('authentication error')
-  }
+  async verify(
+    data: O.Optional<CompulsoryQueryLogin, "password">
+  ): Promise<boolean> {
+    const user = await this.userRepository.find(
+      data.email ? { email: data.email } : { username: data.username }
+    );
+    if (!user) {
+      throw new AuthenticationError("authentication error");
+    }
 
-  return true
+    return true;
   }
 }
 
-export default VerifyAccount
+export default VerifyAccount;
