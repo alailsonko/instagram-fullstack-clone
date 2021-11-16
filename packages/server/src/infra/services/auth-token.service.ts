f14c027ed7d72aa041069fdc1b558121de2bffae
alailson <aalailson3@gmail.com>
2021-11-16 01:33:14 -0300

    refactor

--- packages/server/src/infra/services/auth-token.service.ts
+++ packages/server/src/infra/services/auth-token.service.ts
@@ -1,30 +1,36 @@
-import { randomUUID } from 'crypto'
-import * as JWT from 'jsonwebtoken'
+import { randomUUID } from "crypto";
+import * as JWT from "jsonwebtoken";
 
 type Sub = {
-  id: number,
-  email: string,
-  username: string,
-  uuid: string,
-}
+  id: number;
+  email: string;
+  username: string;
+  uuid: string;
+};
 
-type JWTResponse = Sub & JWT.JwtPayload
+export type JWTResponse = Sub & JWT.JwtPayload;
 
 interface IAuthToken {
-  generate(data: object): Promise<string> 
-  verify (data: string): Promise<string | JWTResponse> 
-} 
+  generate(data: object): Promise<string>;
+  verify(data: string): Promise<string | JWTResponse>;
+}
 
 class AuthToken implements IAuthToken {
- async generate(data: Sub): Promise<string> {
-      return JWT.sign(data, process.env.JWT_SECRET as string, {
-        expiresIn: '1d',
-        jwtid: randomUUID()
-      })
+  async generate(data: Sub): Promise<string> {
+    return JWT.sign(data, process.env.JWT_SECRET as string, {
+      expiresIn: "1d",
+      jwtid: randomUUID(),
+    });
   }
   async verify(data: string): Promise<string | JWTResponse> {
-    return JWT.verify(data, process.env.JWT_SECRET as string) as JWTResponse | string
+    try {
+      return JWT.verify(data, process.env.JWT_SECRET as string) as
+        | JWTResponse
+        | string;
+    } catch (error: any) {
+      return error.message;
+    }
   }
 }
 
-export default AuthToken
+export default AuthToken;
