9181b353279faeca53c6d0cf3c936d20d1a81240
alailson <aalailson3@gmail.com>
2021-11-15 22:07:14 -0300

    draft create post

--- packages/server/src/main/graphql/resolvers/PostResolver.ts
+++ packages/server/src/main/graphql/resolvers/PostResolver.ts
@@ -1,14 +1,16 @@
-import { IResolvers } from 'graphql-tools'
-import {
-  MutationCreatePostArgs,
-  File
-} from '../generated'
-import {
-  GraphQLUpload,
-  FileUpload
-} from 'graphql-upload'
-import { finished } from 'stream/promises'
-import path from 'path'
+import { IResolvers } from "graphql-tools";
+import { MutationCreatePostArgs, File } from "../generated";
+import { GraphQLUpload, FileUpload } from "graphql-upload";
+import { finished } from "stream/promises";
+import path from "path";
+import fs from "fs";
+import { Prisma } from "@prisma/client";
+import http from "http";
+import AuthToken from "../../../infra/services/auth-token.service";
+import Authorization from "../../../infra/middleware/is-authenticated.middleware";
+import { authorization } from "../../../presentation/factories/middleware/authorization.factory";
+import { ContextGraphQL } from "../../../domain/auth/context";
+import { AuthenticationError } from "apollo-server-express";
 
 export const PostResolvers: IResolvers = {
   // Query: {
@@ -20,36 +22,45 @@ export const PostResolvers: IResolvers = {
   //     const signinController = makeSignInController()
   //     return await signinController.handle(args, ctx)
   //   }
-  // },    
+  // },
   Upload: GraphQLUpload,
   Mutation: {
-    async createPost (
+    async createPost(
       _: undefined,
       args: MutationCreatePostArgs,
-      ctx
+      ctx: ContextGraphQL
     ): Promise<File> {
-     const files = args.file as FileUpload[]
-     for await (const file of files ) {
-       const {  createReadStream, filename, mimetype, encoding } = file
-       // Invoking the `createReadStream` will return a Readable Stream.
-      // See https://nodejs.org/api/stream.html#stream_readable_streams
-      const stream = createReadStream();
+      if (!(await authorization().isLogged(ctx.token))) {
+        throw new AuthenticationError("must be logged in.");
+      }
+      const files = args.file as FileUpload[];
+      const medias = [] as Prisma.MediaCreateInput[];
+      const post = {} as Prisma.PostCreateInput;
+      for await (const file of files) {
+        const { createReadStream, filename, mimetype, encoding } = file;
+        const stream = createReadStream();
+        const __dirname = path.resolve();
+        const fileName = `${+new Date()}_${filename}`;
+        const filePath = path.join(__dirname, "src", "uploads/") + fileName;
+        const out = fs.createWriteStream(filePath);
+        stream.pipe(out);
+        await finished(out);
 
-      // This is purely for demonstration purposes and will overwrite the
-      // local-file-output.txt in the current working directory on EACH upload.
-      const __dirname = path.resolve()
-      const out = require('fs').createWriteStream(__dirname + filename);
-      stream.pipe(out);
-      await finished(out);
-      // return await signupController.handle(args, ctx)
-
-      // return { filename, mimetype, encoding };     
-     }  
-     return {
-       filename: 'somename',
-       encoding: 'some',
-       mimetype: 'some'
-     }
-    }
-  }
-}
+        medias.push({
+          url: filePath,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+      }
+      Object.assign(post, {
+        description: args.description,
+        medias,
+      });
+      return {
+        filename: "somename",
+        encoding: "some",
+        mimetype: "some",
+      };
+    },
+  },
+};
