6c45be603a9a391241e0e5d91df44f21b7928ca7
alailson <aalailson3@gmail.com>
2021-11-15 22:07:40 -0300

    authorization factory

---
+++ packages/server/src/presentation/factories/middleware/authorization.factory.ts
@@ -0,0 +1,14 @@
+import prisma from "../../../infra/db/prisma/prisma.helper"
+import Authorization from "../../../infra/middleware/is-authenticated.middleware"
+import UserRepository from "../../../infra/repositories/users/users.repositories"
+import AuthToken from "../../../infra/services/auth-token.service"
+
+function authorization() {
+   const userRepository = new UserRepository(prisma)
+   const authToken = new AuthToken()
+   return new Authorization(userRepository, authToken)
+}
+
+export {
+  authorization
+}
