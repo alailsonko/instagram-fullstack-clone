4425b0d2a1e394d5b615510be7028649d304a925
alailson <aalailson3@gmail.com>
2021-11-16 01:32:30 -0300

    update context

--- packages/server/src/domain/auth/context.ts
+++ packages/server/src/domain/auth/context.ts
@@ -1,6 +1,9 @@
 import { PubSub } from "graphql-subscriptions";
+import { JWTResponse } from "../../infra/services/auth-token.service";
 
 export interface ContextGraphQL {
   token: string,
-  pubsub: PubSub
+  pubsub: PubSub,
+  isLogged: boolean,
+  user: JWTResponse | null | string
 }
