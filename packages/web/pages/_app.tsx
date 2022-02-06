472360344dbc952ee2af296e4e27eafcfb1b8263
alailson <aalailson3@gmail.com>
2022-02-06 12:00:32 -0300

    set chakra ui and recoil

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { RecoilRoot } from "recoil";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import React from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  React.useLayoutEffect = React.useEffect
   return (
-    <ChakraProvider>
-      <RecoilRoot>
+    <RecoilRoot>
+      <ChakraProvider>
         <Component {...pageProps} />
-      </RecoilRoot>
-    </ChakraProvider>
+      </ChakraProvider>
+    </RecoilRoot>
   );
 }
 
