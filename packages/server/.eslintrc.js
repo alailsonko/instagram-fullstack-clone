86a89031c2579764955b3021abdf3096c9eae25c
alailson <aalailson3@gmail.com>
2022-02-20 21:20:06 -0300

    add rules lint

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -1,11 +1,23 @@
 module.exports = {
   extends: 'standard-with-typescript',
   parserOptions: {
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   rules: {
     '@typescript-eslint/restrict-template-expressions': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/no-misused-promises': 'off'
-  }
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/prefer-readonly': 'off',
+    'comma-dangle': [
+      2,
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      },
+    ],
+  },
 }
