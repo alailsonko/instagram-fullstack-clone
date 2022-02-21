0b14989bfd82d69e72cfd60aa28980a765b92611
alailson <aalailson3@gmail.com>
2022-02-20 21:20:29 -0300

    lint

--- packages/server/src/main/server.ts
+++ packages/server/src/main/server.ts
@@ -1,22 +1,22 @@
-import { ApolloServer, ExpressContext } from "apollo-server-express";
-import express from "express";
-import schemaOne from "./graphql/schemasMap";
-import dotenv from "dotenv";
-import { ContextGraphQL } from "../domain/auth/context";
-import { SubscriptionServer } from "subscriptions-transport-ws";
-import { execute, subscribe } from "graphql";
-import { createServer } from "http";
-import { PubSub } from "graphql-subscriptions";
-import { graphqlUploadExpress } from "graphql-upload";
-import path from "path";
-import { makeAuthorization } from "../presentation/factories/middleware/authorization.factory";
-import { mergeSchemas } from "graphql-tools";
-import schemeTwo from "./graphql/resolvers/relay.schema";
+import { ApolloServer, ExpressContext } from 'apollo-server-express'
+import express from 'express'
+import schemaOne from './graphql/schemasMap'
+import dotenv from 'dotenv'
+import { ContextGraphQL } from '../domain/auth/context'
+import { SubscriptionServer } from 'subscriptions-transport-ws'
+import { execute, subscribe } from 'graphql'
+import { createServer } from 'http'
+import { PubSub } from 'graphql-subscriptions'
+import { graphqlUploadExpress } from 'graphql-upload'
+import path from 'path'
+import { makeAuthorization } from '../presentation/factories/middleware/authorization.factory'
+import { mergeSchemas } from 'graphql-tools'
+import schemeTwo from './graphql/resolvers/relay.schema'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const httpServer = createServer(app);
+const app = express()
+const httpServer = createServer(app)
 
 const server = new ApolloServer({
   uploads: false,
@@ -25,10 +25,10 @@ const server = new ApolloServer({
   }),
   subscriptions: {
     onConnect: (connectionParams, webSocket, context) => {
-      console.log("Client connected");
+      console.log('Client connected')
     },
     onDisconnect: (webSocket, context) => {
-      console.log("Client disconnected");
+      console.log('Client disconnected')
     },
   },
   plugins: [
@@ -36,30 +36,30 @@ const server = new ApolloServer({
       async serverWillStart() {
         return {
           serverWillStop() {
-            console.log("hello");
-            subscriptionServer.close();
+            console.log('hello')
+            subscriptionServer.close()
           },
-        };
+        }
       },
     },
     {
       requestDidStart() {
         return {
           executionDidStart() {
-            console.log("started");
+            console.log('started')
           },
-        };
+        }
       },
     },
   ],
   context: async ({ req }: ExpressContext): Promise<ContextGraphQL> => {
-    const pubsub = new PubSub();
-    const authorization = makeAuthorization();
-    const [, token] = req.headers.authorization?.split(" ") ?? "";
-    const [isLogged, user] = await authorization.isLogged(token);
-    return { token, pubsub, isLogged, user };
+    const pubsub = new PubSub()
+    const authorization = makeAuthorization()
+    const [, token] = req.headers.authorization?.split(' ') ?? ''
+    const [isLogged, user] = await authorization.isLogged(token)
+    return { token, pubsub, isLogged, user }
   },
-});
+})
 
 const subscriptionServer = SubscriptionServer.create(
   {
@@ -71,21 +71,21 @@ const subscriptionServer = SubscriptionServer.create(
     onConnect: () => {
       return {
         pubsub: new PubSub(),
-      };
+      }
     },
   },
   {
     server: httpServer,
     path: server.graphqlPath,
   }
-);
+)
 
-app.use(graphqlUploadExpress());
-app.use(express.static(path.join(__dirname, "..", "uploads")));
+app.use(graphqlUploadExpress())
+app.use(express.static(path.join(__dirname, '..', 'uploads')))
 
-server.applyMiddleware({ app, path: "/graphql" });
+server.applyMiddleware({ app, path: '/graphql' })
 
 httpServer.listen(process.env.PORT, () => {
-  console.log(`listening on ${process.env.PORT}`);
-  console.log(`listening ws on ${server.subscriptionsPath}`);
-});
+  console.log(`listening on ${process.env.PORT}`)
+  console.log(`listening ws on ${server.subscriptionsPath}`)
+})
--- packages/server/src/presentation/controllers/signup/signup.controller.ts
+++ packages/server/src/presentation/controllers/signup/signup.controller.ts
@@ -1,73 +1,70 @@
-import { ContextGraphQL } from "../../../domain/auth/context";
-import {
-  MutationRegisterArgs,
-  AuthenticateResponse,
-} from "../../../main/graphql/generated";
-import { O } from "ts-toolbelt";
-import { Controller } from "./../controller.protocol";
-import { UserInputError, AuthenticationError } from "apollo-server";
-import AddAccount from "../../../data/usecases/add-account.usecases";
-import AuthToken from "../../../infra/services/auth-token.service";
-import { slugifyParser } from "../../../utils/slugify";
-import validator from "validator";
+import { ContextGraphQL } from '../../../domain/auth/context'
+import { MutationRegisterArgs, AuthenticateResponse } from '../../../main/graphql/generated'
+import { O } from 'ts-toolbelt'
+import { Controller } from './../controller.protocol'
+import { UserInputError, AuthenticationError } from 'apollo-server'
+import AddAccount from '../../../data/usecases/add-account.usecases'
+import AuthToken from '../../../infra/services/auth-token.service'
+import { slugifyParser } from '../../../utils/slugify'
+import validator from 'validator'
 
-export type SignUpResponse = AuthenticateResponse;
+export type SignUpResponse = AuthenticateResponse
 
 interface INDEX_ARGS_Signature {
-  email: "email";
-  password: "password";
-  username: "username";
-  passwordConfirm: "passwordConfirm";
+  email: 'email'
+  password: 'password'
+  username: 'username'
+  passwordConfirm: 'passwordConfirm'
 }
 
 export default class SignUpController
-  implements Controller<SignUpResponse, MutationRegisterArgs, ContextGraphQL>
-{
-  private addAccount: AddAccount;
-  private authToken: AuthToken;
+implements Controller<SignUpResponse, MutationRegisterArgs, ContextGraphQL> {
+  private addAccount: AddAccount
+  private authToken: AuthToken
   constructor(addAccount: AddAccount, authToken: AuthToken) {
-    this.addAccount = addAccount;
-    this.authToken = authToken;
+    this.addAccount = addAccount
+    this.authToken = authToken
   }
+
   async handle(
-    args: O.Optional<MutationRegisterArgs, "passwordConfirm">,
+    args: O.Optional<MutationRegisterArgs, 'passwordConfirm'>,
     ctx: ContextGraphQL
   ): Promise<SignUpResponse> {
-    const fields = ["email", "username", "password", "passwordConfirm"] as [
-      INDEX_ARGS_Signature["email"],
-      INDEX_ARGS_Signature["username"],
-      INDEX_ARGS_Signature["password"],
-      INDEX_ARGS_Signature["passwordConfirm"]
-    ];
+    const fields = ['email', 'username', 'password', 'passwordConfirm'] as [
+      INDEX_ARGS_Signature['email'],
+      INDEX_ARGS_Signature['username'],
+      INDEX_ARGS_Signature['password'],
+      INDEX_ARGS_Signature['passwordConfirm']
+    ]
 
     for (const field of fields) {
       if (!(args as INDEX_ARGS_Signature)[field]) {
-        throw new UserInputError(`${field} must not be empty.`);
+        throw new UserInputError(`${field} must not be empty.`)
       }
     }
 
     if (!validator.isEmail(args.email)) {
-      throw new UserInputError("must be a valid email.");
+      throw new UserInputError('must be a valid email.')
     }
 
     if (args.password !== args.passwordConfirm) {
-      throw new AuthenticationError("authentication error.");
+      throw new AuthenticationError('authentication error.')
     }
-    delete args.passwordConfirm;
-    slugifyParser(args.username);
+    delete args.passwordConfirm
+    slugifyParser(args.username)
     const user = await this.addAccount.add(
       Object.assign(args, {
         username: slugifyParser(args.username),
         createdAt: new Date(),
         updatedAt: new Date(),
       })
-    );
+    )
     const token = await this.authToken.generate({
       email: user.email,
       username: user.username,
       id: user.id,
       uuid: user.uuid,
-    });
+    })
     return {
       user: {
         id: user.id,
@@ -75,9 +72,9 @@ export default class SignUpController
         updatedAt: user.updatedAt,
         email: user.email,
         username: user.username,
-        uuid: user.uuid
+        uuid: user.uuid,
       },
       token,
-    };
+    }
   }
 }
