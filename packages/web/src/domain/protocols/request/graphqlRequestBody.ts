ce46f977995a626fe0f6b28e8d7230a5b0e8f576
alailson <aalailson3@gmail.com>
2022-04-17 13:46:04 -0300

    add graphqlRequestbody for fetching

---
+++ packages/web/src/domain/protocols/request/graphqlRequestBody.ts
@@ -0,0 +1,12 @@
+type GraphqlRequestBodyType<T> = {
+  query: string;
+  variables: T;
+};
+
+export const graphqlRequestBody = <V extends Object>(
+  query: string,
+  variables = {} as V
+): GraphqlRequestBodyType<V> => ({
+  query,
+  variables
+});
