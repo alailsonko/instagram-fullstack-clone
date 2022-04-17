ca933f61837d84ed7fab3d940a0ab5f43e742a23
alailson <aalailson3@gmail.com>
2022-04-17 20:53:38 -0300

    add spinner and toasts

--- packages/web/src/presentation/sections/SignUpSection/index.tsx
+++ packages/web/src/presentation/sections/SignUpSection/index.tsx
@@ -1,3 +1,4 @@
+import { useToast } from '@chakra-ui/react';
 import { config } from 'config/config';
 import {
   signUpMutation,
@@ -6,16 +7,25 @@ import {
 import { graphqlRequestBody } from 'domain/protocols/request/graphqlRequestBody';
 import { HTTP_METHODS_ENUM } from 'domain/protocols/request/httpMethodsEnum';
 import { SignUpFormProps, SignUpResponse, SubmitSignUpHandler } from 'domain/usecases/signup';
+import { SpinnerFeedback } from 'infra/components/Feedback/Spinner';
 import { BoxLayout } from 'infra/components/Layout/Box';
 import { VStackLayout } from 'infra/components/Layout/Stack';
 import { LinkNavigation } from 'infra/components/Navigation/Link';
 import { request } from 'infra/services/httpRequest/request';
 import InstagramLogoBlock from 'presentation/blocks/InstagramLogoBlock';
 import SignUpFormBlock from 'presentation/blocks/SignUpFormBlock';
+import { useEffect } from 'react';
 import { useMutation } from 'react-query';
 
 const SignUpSection = () => {
-  const signUpMutationResponse = useMutation(async (data: SignUpFormProps) => {
+  const toast = useToast();
+  const {
+    mutateAsync,
+    isSuccess,
+    isLoading,
+    isError
+    // data: dataMutation
+  } = useMutation(async (data: SignUpFormProps) => {
     return request<SignUpResponse>(
       config.GRAPHQL_ENDPOINT,
       HTTP_METHODS_ENUM.POST,
@@ -31,15 +41,29 @@ const SignUpSection = () => {
     );
   });
   const handleSignUpSubmit: SubmitSignUpHandler = async (data) => {
-    const response = await signUpMutationResponse.mutateAsync(data);
-    console.log('response', response);
-    console.log('response', {
-      isLoading: signUpMutationResponse.isLoading,
-      isSuccess: signUpMutationResponse.isSuccess,
-      dataMutation: signUpMutationResponse.data,
-      isError: signUpMutationResponse.isError
-    });
+    return mutateAsync(data);
   };
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast({
+        title: 'Account created.',
+        description: "We're redirecting you.",
+        status: 'success',
+        duration: 9000,
+        isClosable: true
+      });
+    }
+    if (isError) {
+      toast({
+        title: 'Error while creating account.',
+        description: 'Please try again few minutes later.',
+        status: 'error',
+        duration: 9000,
+        isClosable: true
+      });
+    }
+  }, [isSuccess, isError]);
 
   return (
     <VStackLayout>
@@ -52,24 +76,38 @@ const SignUpSection = () => {
         width="30rem"
         height="25rem">
         <InstagramLogoBlock />
-        <SignUpFormBlock onSubmit={handleSignUpSubmit} />
+        {isLoading || isSuccess ? (
+          <SpinnerFeedback
+            alignSelf="center"
+            marginTop="30%"
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        ) : (
+          <SignUpFormBlock onSubmit={handleSignUpSubmit} />
+        )}
       </BoxLayout>
-      <BoxLayout
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        p={4}
-        borderWidth={1}
-        width="30rem"
-        height="4rem">
-        <LinkNavigation
-          to="/"
-          style={{
-            justifyContent: 'center'
-          }}>
-          Sign In
-        </LinkNavigation>
-      </BoxLayout>
+      {!isSuccess && (
+        <BoxLayout
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          p={4}
+          borderWidth={1}
+          width="30rem"
+          height="4rem">
+          <LinkNavigation
+            to="/"
+            style={{
+              justifyContent: 'center'
+            }}>
+            Sign In
+          </LinkNavigation>
+        </BoxLayout>
+      )}
     </VStackLayout>
   );
 };
