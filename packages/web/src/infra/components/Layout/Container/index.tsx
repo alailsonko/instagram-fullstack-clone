00da4b36d7f75d566aa394a006bba4d7b5d2ca34
alailson <aalailson3@gmail.com>
2022-04-23 23:29:43 -0300

    implements page transition animation

--- packages/web/src/infra/components/Layout/Container/index.tsx
+++ packages/web/src/infra/components/Layout/Container/index.tsx
@@ -1,7 +1,31 @@
 import { Container, ContainerProps } from '@chakra-ui/react';
 import { FC } from 'react';
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.5
+    }
+  },
+  exit: {
+    x: '-100vh',
+    transition: {
+      ease: 'easeInOut'
+    }
+  }
+};
 
 export const ContainerLayout: FC<ContainerProps> = (props) => {
   const { children } = props;
-  return <Container {...props}>{children}</Container>;
+  return (
+    <motion.div variants={containerVariants} initial="hidden" animate="visible" exit="exit">
+      <Container {...props}>{children}</Container>
+    </motion.div>
+  );
 };
--- packages/web/src/main/routes/index.tsx
+++ packages/web/src/main/routes/index.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from 'main/pages/Home';
 import SignUp from 'main/pages/SignUp';
+import { AnimatePresence } from 'framer-motion';
 
 function RoutesApp() {
+  // const location = useLocation();
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/accounts/emailsignup" element={<SignUp />} />
-      </Routes>
+      <AnimatePresence exitBeforeEnter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/accounts/emailsignup" element={<SignUp />} />
+        </Routes>
+      </AnimatePresence>
     </BrowserRouter>
   );
 }
