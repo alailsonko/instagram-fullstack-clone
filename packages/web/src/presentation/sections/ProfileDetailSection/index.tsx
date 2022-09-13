96e684c679ce3b3d68946537638b190cacb447fc
alailson <aalailson3@gmail.com>
2022-09-13 15:51:41 -0300

    get profile by slug

--- packages/web/src/presentation/sections/ProfileDetailSection/index.tsx
+++ packages/web/src/presentation/sections/ProfileDetailSection/index.tsx
@@ -1,16 +1,101 @@
-import React, { useEffect } from 'react';
+import React, { FC, useEffect } from 'react';
 import { HStackLayout, VStackLayout } from 'infra/components/Layout/Stack';
 import { BoxLayout } from 'infra/components/Layout/Box';
-import { Avatar, Flex, Grid, Text } from '@chakra-ui/react';
+import { Avatar, Flex, Grid, GridItem, Image, Text } from '@chakra-ui/react';
 import { Btn } from 'infra/components/Forms/Button';
 import { useFetchQuery } from 'infra/hooks/useFetchQuery';
 import { GetProfileBySlugResponse } from 'domain/usecases/getProfileBySlug';
+import graphql from 'babel-plugin-relay/macro';
 import {
   getProfileBySlugQuery,
   getProfileBySlugQueryVariables,
   GetProfileBySlugVariablesType
 } from 'domain/models/graphql/users/query/getProfileBySlug';
 import { useParams } from 'react-router-dom';
+import { PreloadedQuery, usePreloadedQuery, useQueryLoader } from 'react-relay';
+import { ProfileDetailSectionQuery } from './__generated__/ProfileDetailSectionQuery.graphql';
+
+const GET_POSTS_BY_SLUG = graphql`
+  query ProfileDetailSectionQuery(
+    $first: Int
+    $before: String
+    $last: Int
+    $username: String
+    $after: String
+  ) {
+    getPostsBySlug(
+      first: $first
+      before: $before
+      last: $last
+      username: $username
+      after: $after
+    ) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          idSerial
+          user {
+            idSerial
+            username
+            email
+            createdAt
+            updatedAt
+            avatar {
+              idSerial
+              url
+              postId
+              createdAt
+              updatedAt
+              id
+            }
+            avatarId
+            id
+          }
+          userId
+          description
+          medias {
+            idSerial
+            url
+            postId
+            createdAt
+            updatedAt
+            id
+          }
+          createdAt
+          id
+          updatedAt
+        }
+        cursor
+      }
+    }
+  }
+`;
+
+type Props = {
+  initialQueryRef: PreloadedQuery<ProfileDetailSectionQuery>;
+};
+
+const ProfilePostsList: FC<Props> = (props) => {
+  const { initialQueryRef } = props;
+  const data = usePreloadedQuery<ProfileDetailSectionQuery>(GET_POSTS_BY_SLUG, initialQueryRef);
+
+  return (
+    <Grid templateColumns="repeat(1, 1fr 1fr 1fr)">
+      {data.getPostsBySlug.edges?.map((item) => (
+        <GridItem cursor="pointer" padding="2">
+          <Image
+            src={`${process.env.REACT_APP_STATIC_FILES_URL}/static/${item?.node?.medias[0]?.url}`}
+          />
+        </GridItem>
+      ))}
+    </Grid>
+  );
+};
 
 const ProfileDetailSection = () => {
   const { slug } = useParams<{ slug: string }>();
@@ -21,6 +106,9 @@ const ProfileDetailSection = () => {
     isSuccess,
     isError
   } = useFetchQuery<GetProfileBySlugVariablesType, GetProfileBySlugResponse>();
+  const [getPostsBySlugQueryReference, getPostsBySlugLoadQuery] =
+    useQueryLoader<ProfileDetailSectionQuery>(GET_POSTS_BY_SLUG);
+
   useEffect(() => {
     if (slug) {
       dispatchFetch({
@@ -30,7 +118,9 @@ const ProfileDetailSection = () => {
         })
       });
     }
+    getPostsBySlugLoadQuery({});
   }, []);
+
   return (
     <BoxLayout>
       <Grid templateColumns="repeat(1, 0.7fr 1fr)">
@@ -74,6 +164,9 @@ const ProfileDetailSection = () => {
           </HStackLayout>
         </VStackLayout>
       </Grid>
+      {getPostsBySlugQueryReference && (
+        <ProfilePostsList initialQueryRef={getPostsBySlugQueryReference} />
+      )}
     </BoxLayout>
   );
 };
