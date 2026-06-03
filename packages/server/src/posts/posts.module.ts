650ce6d95e2f7e97c6bef4e78b744a58ad88f637
alailsonko <aalailson3@gmail.com>
2026-06-03 20:48:39 +0200

    deleting

--- packages/server/src/posts/posts.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostsService } from './services/posts.service';
-
-@Module({
-  providers: [PostsService],
-  exports: [PostsService],
-})
-export class PostsModule {}
